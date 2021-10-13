6b8942c2fc1e23afe718f6265528aada681ff5c2
Katarina Vucic <vucic.kat@gmail.com>
2021-10-13 19:29:47 +0200

    logovanje na index.php

--- index.php
+++ index.php
@@ -1,18 +1,54 @@
+<?php
+require "dbBroker.php";
+require "model/user.php";
+
+session_start();
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    $uname = $_POST['username'];
+    $upass = $_POST['password'];
+
+
+    $korisnik = new User(null, $uname, $upass);
+    // $odg = $korisnik->loginUser($uname, $upass, $conn);
+    $odg = User::loginUser($korisnik, $conn); // posto smo login metodu definisali kao staticku moze i ovako
+
+    if ($odg->num_rows == 1) {
+        echo `
+        <script>
+        console.log("Uspešno ste se ulogovali!");
+        </script>
+        `;
+        $_SESSION['user_id'] = $korisnik->id;
+        header('Location: home.php');
+        exit();
+    } else {
+        echo `
+        <script>
+        console.log("Niste se ulogovali!");
+        </script>
+        `;
+    }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <title>FON: Zakazivanje kolokvijuma</title>
 
 </head>
+
 <body>
     <div class="login-form">
         <div class="main-div">
             <form method="POST" action="#">
                 <div class="container">
                     <label class="username">Korisnicko ime</label>
-                    <input type="text" name="username" class="form-control"  required>
+                    <input type="text" name="username" class="form-control" required>
                     <br>
                     <label for="password">Lozinka</label>
                     <input type="password" name="password" class="form-control" required>
@@ -22,7 +58,8 @@
             </form>
         </div>
 
-        
+
     </div>
 </body>
+
 </html>
