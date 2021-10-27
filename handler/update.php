27374a036d9ec9ebe13f7b91957cc499378256d1
Katarina Vucic <vucic.kat@gmail.com>
2021-10-27 18:46:17 +0200

    delimicno implementirana update funkcija

---
+++ handler/update.php
@@ -0,0 +1,5 @@
+<?php
+
+// kreirati  update funkciju
+
+?> 
--- js/main.js
+++ js/main.js
@@ -57,3 +57,100 @@ $('#btn-obrisi').click(function() {
     });
 
 });
+
+// dugme koje je na glavnoj formi i otvara dijalog za izmenu
+$('#btn-izmeni').click(function() {
+    const checked = $('input[name=checked-donut]:checked');
+    //pristupa informacijama te konkretne forme i popunjava dijalog
+    request = $.ajax({
+        url: 'handler/get.php',
+        type: 'post',
+        data: { 'id': checked.val() },
+        dataType: 'json'
+    });
+
+
+    request.done(function(response, textStatus, jqXHR) {
+        console.log('Popunjena');
+        $('#predmet').val(response[0]['predmet']);
+        console.log(response[0]['predmet']);
+
+        $('#katedra').val(response[0]['katedra'].trim());
+        console.log(response[0]['katedra'].trim());
+
+        $('#sala').val(response[0]['sala'].trim());
+        console.log(response[0]['sala'].trim());
+
+        $('#datum').val(response[0]['datum'].trim());
+        console.log(response[0]['datum'].trim());
+
+        $('#id').val(checked.val());
+
+        console.log(response);
+    });
+
+    request.fail(function(jqXHR, textStatus, errorThrown) {
+        console.error('The following error occurred: ' + textStatus, errorThrown);
+    });
+
+});
+
+//dugme za slanje UPDATE zahteva nakon popunjene forme
+$('#izmeniForm').submit(function() {
+    event.preventDefault();
+    console.log("Izmene");
+    const $form = $(this);
+    const $inputs = $form.find('input, select, button, textarea');
+    const serializedData = $form.serialize();
+    console.log(serializedData);
+    $inputs.prop('disabled', true);
+
+    // kreirati request za UPDATE handler
+
+    request.done(function(response, textStatus, jqXHR) {
+
+
+        if (response === 'Success') {
+            console.log('Kolokvijum je izmenjen');
+            location.reload(true);
+            //$('#izmeniForm').reset;
+        } else console.log('Kolokvijum nije izmenjen ' + response);
+        console.log(response);
+    });
+
+    request.fail(function(jqXHR, textStatus, errorThrown) {
+        console.error('The following error occurred: ' + textStatus, errorThrown);
+    });
+
+
+    //$('#izmeniModal').modal('hide');
+});
+
+$('#btn-pretraga').click(function() {
+
+    var para = document.querySelector('#myInput');
+    console.log(para);
+    var style = window.getComputedStyle(para);
+    console.log(style);
+    if (!(style.display === 'inline-block') || ($('#myInput').css("visibility") == "hidden")) {
+        console.log('block');
+        $('#myInput').show();
+        document.querySelector("#myInput").style.visibility = "";
+    } else {
+        document.querySelector("#myInput").style.visibility = "hidden";
+    }
+});
+
+$('#btn').click(function() {
+    $('#pregled').toggle();
+});
+
+$('#btnDodaj').submit(function() {
+    $('#myModal').modal('toggle');
+    return false;
+});
+
+$('#btnIzmeni').submit(function() {
+    $('#myModal').modal('toggle');
+    return false;
+});
